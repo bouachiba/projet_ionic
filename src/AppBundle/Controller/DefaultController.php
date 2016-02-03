62fab292301affc755aa9be08d5445e728b19e85
Sebastien Maloron <contact@smaloron.eu>
2016-02-03 20:32:48 +0100

    Ajout des gabarits et des contrôleurs pour constituer le squelette du projet

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,31 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @return Response
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ));
+
+        return $this->render(
+            'default/index.html.twig',
+            array()
+        );
+    }
+
+    /**
+     * @Route("/about", name="about_page")
+     * @return Response
+     */
+    public function aboutAction(){
+        return $this->render(
+            'static/about.html.twig',
+            array()
+        );
     }
 }
