f337123fd1b545db4c39fcda375f9f2fd11ade10
Sebastien Maloron <contact@smaloron.eu>
2016-02-06 15:32:03 +0100

    Déplacement de l'action author/new du contrôleur AuthorController vers le contrôleur AdminController la nouvelle route est /admin/author/new Ceci évite le login auteur pour créer un nouvel auteur

--- src/AppBundle/Controller/AuthorController.php
+++ src/AppBundle/Controller/AuthorController.php
@@ -35,36 +35,4 @@ class AuthorController extends Controller
     {
         return $this->render('author/form.html.twig');
     }
-
-    /**
-     * @Route("/new", name="author_new")
-     * @return Response
-     */
-    public function newAction(Request $request)
-    {
-        $author = new Author();
-
-        //Création d'un nouveau formulaire
-        $form = $this->createForm(AuthorType::class,
-            $author,
-            array(
-                'action' => $this->generateUrl('author_new')
-            )
-        );
-
-        //Hydratation du formulaire avec les données de la requête
-        $form->handleRequest($request);
-
-        //Traitement du formulaire s'il est valide
-        if($form->isValid()){
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($author);
-            $em->flush();
-
-            return $this->redirectToRoute('admin_home');
-        }
-
-        //Affichage du formulaire
-        return $this->render('author/form.html.twig', array('authorForm' => $form->createView()));
-    }
 }
